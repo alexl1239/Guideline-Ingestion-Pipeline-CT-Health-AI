c7c7f6bb87dfbcd2559b05500b7374ba6495bdf2
alexl1239 <aluo27@choate.edu>
2025-11-29 23:51:07 -0500

    Added more dependencies

---
+++ configs.py
@@ -0,0 +1,14 @@
+# ==================================
+# Pipeline Configuration (Numeric/Model Settings)
+# ==================================
+EMBEDDING_MODEL_NAME="text-embedding-3-small"
+EMBEDDING_DIMENSION="1536"
+CHUNK_TOKEN_TARGET="256"
+PARENT_TOKEN_TARGET="1500"
+
+# ==================================
+# File Paths (Input / Output Locations)
+# ==================================
+SOURCE_PDF_PATH1="ucg23_raw/Uganda_Clinical_Guidelines_2023-part-1.pdf"
+SOURCE_PDF_PATH2="ucg23_raw/Uganda_Clinical_Guidelines_2023-part-2.pdf"
+DATABASE_PATH="data/ucg23_rag.db"
